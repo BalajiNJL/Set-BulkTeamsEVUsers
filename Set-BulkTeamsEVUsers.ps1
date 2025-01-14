c32ba74ac39c58e18520bcd5f689987137f7e752
Balaji NJL <balaji@drishtic.ai>
2025-01-13 22:05:28 -0800

    added parameters to input

--- Set-BulkTeamsEVUsers.ps1
+++ Set-BulkTeamsEVUsers.ps1
@@ -1,9 +1,47 @@
+# ============================================================================
+# Set-BulkTeamsEVUsers.ps1
+# ============================================================================
 # Script to bulk enable Teams Phone system (EV) for users
+#
+# This script is designed to bulk enable Teams Phone System (Enterprise Voice) for users using a CSV file as input.
+# It connects to Microsoft Teams Online and performs necessary configurations for each user listed in the CSV.
+#
+# Usage:
+#   .\Set-BulkTeamsEVUsers.ps1 -msFQDN <tenant_domain> -CSVFilePath <path_to_csv_file>
+#
+# Parameters:
+#   -msFQDN: The fully qualified domain name (FQDN) for the tenant's onmicrosoft.com domain (e.g., example.onmicrosoft.com)
+#   -CSVFilePath: The path to the CSV file containing the user data (e.g., c:\path_to_file\TeamsEVUsersTemplate.csv)
+#
+# CSV File Format: (refer to TeamsEVUsersTemplate.csv)
+#   The CSV file should contain the following columns:
+#     - UPN: User Principal Name
+#     - PhoneNumber: Direct phone number with extension if applicable
+#     - OnlineVoiceRoutingPolicy: Name of the voice routing policy
+#     - Privateline: (Optional) Private line number
+#
+# Example CSV File:
+#   UPN,PhoneNumber,OnlineVoiceRoutingPolicy,Privateline
+#   user1@example.com,+12065551234;ext=1234,No Restrictions,+12065556789
+#   user2@example.com,+12065551235;ext=1235,No Restrictions,
+#
+# ============================================================================
+# $msFQDN = "example.onmicrosoft.com"
+# $CSVFilePath = "c:\path_to_file\TeamsEVUsersTemplate.csv"
+
+# define parameters
+param (
+    [string]$msFQDN,
+    [string]$CSVFilePath
+)
+
+if (!$msFQDN -or !$CSVFilePath) {
+    Write-Host "Error: Both msFQDN and CSVFilePath parameters are required."
+    exit 1
+}
 
 # define and set variables
 $userEvEnabled = $true
-$msFQDN = "example.onmicrosoft.com"
-$CSVFilePath = "c:\path_to_file\TeamsEVUsersTemplate.csv"
 
 try {
     # Connect to TeamsOnline
